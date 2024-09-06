76b4a6186f0aa1a19707fc6a8644a5c5195e9f2c
Juliano Firme <julianofirme23@gmail.com>
2024-09-05 21:26:56 -0300

    feat: adding logs

--- src/services/start-container.ts
+++ src/services/start-container.ts
@@ -6,8 +6,10 @@ export async function startContainer(containerId: string) {
   try {
     const container = docker.getContainer(containerId)
     await container.start();
+
+    console.info('✅ Container started!')
   } catch (error) {
-    console.error('Error starting container:', error);
+    console.error('❌ Error starting container:', error);
     throw error;
   }
 }
